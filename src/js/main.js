bdfba56ef6f1b215724192491e02d10bea2cd291
Loreto <loreto0105@gmail.com>
2025-03-21 19:40:45 +0100

    map

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const handleFetch = () => {
+    fetch ("https://randomuser.me/api/?results=10")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const newArray= data.results.map((user)=>{
+            return {
+                name: user.name,
+                userName: user.login.username,
+                city: user.location.city,
+                photo: user.picture.large,
+            }
+        })
+    })
+}
+window.addEventListener("load", handleFetch);
+
