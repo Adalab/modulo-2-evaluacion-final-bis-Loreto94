e6b96efbcb2738a47f5c682042b3aa92ecc20422
Loreto <loreto0105@gmail.com>
2025-03-22 12:42:07 +0100

    renderUsers

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,8 @@
 
 console.log('>> Ready :)');
 
+const usersList= document.querySelector(".js-usersList");
+
 const handleFetch = () => {
     fetch ("https://randomuser.me/api/?results=10")
     .then(response => response.json())
@@ -9,13 +11,29 @@ const handleFetch = () => {
         console.log(data);
         const newArray= data.results.map((user)=>{
             return {
-                name: user.name,
-                userName: user.login.username,
-                city: user.location.city,
                 photo: user.picture.large,
+                name: user.name.title + "" + user.name.first + "" + user.name.last,
+                city: user.location.city,
+                userName: user.login.username,  
             }
         })
+        console.log(newArray);
+        renderUsers(newArray); 
+        function renderUsers(users){
+            let content= "";
+            for (const user of users)
+            content+= `
+            <li>
+                <img src="${user.photo}" alt="">
+                <h2>${user.name}</h2>
+                <p>${user.city}</p>
+                <p>${user.userName}</p>
+            </li>
+            `
+            usersList.innerHTML= content;
+        }
     })
 }
 window.addEventListener("load", handleFetch);
+
 
