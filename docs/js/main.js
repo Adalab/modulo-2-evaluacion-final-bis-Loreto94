8c0dd1b5a37bcdb1eba265f64bf04607a4a7a2aa
Loreto <loreto0105@gmail.com>
2025-03-23 14:17:32 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,9 @@
-console.log(">> Ready :)");const i=document.querySelector(".js-usersList"),a=()=>{fetch("https://randomuser.me/api/?results=10").then(t=>t.json()).then(t=>{console.log(t);const o=t.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1}));console.log(o),l(o);function l(e){let s="";for(const n of e)s+=`
-            <li class="js-user">
+console.log(">> Ready :)");const i=document.querySelector(".js-usersList"),a=()=>{fetch("https://randomuser.me/api/?results=10").then(t=>t.json()).then(t=>{console.log(t);const s=t.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value}));console.log(s),l(s);function l(e){let o="";for(const n of e)o+=`
+            <li class="js-user" id="${n.idValue}">
                 <img src="${n.photo}" alt="">
                 <h2>${n.name}</h2>
                 <p>${n.city}</p>
                 <p>${n.userName}</p>
             </li>
-            `;i.innerHTML=s;const r=document.querySelectorAll(".js-user");for(const n of r)n.addEventListener("click",c)}function c(){console.log("Ha hecho click")}})};window.addEventListener("load",a);
+            `;i.innerHTML=o;const r=document.querySelectorAll(".js-user");for(const n of r)n.addEventListener("click",c)}function c(e){console.log("Ha hecho click");const o=e.currentTarget.id;console.log(o)}})};window.addEventListener("load",a);
 //# sourceMappingURL=main.js.map
