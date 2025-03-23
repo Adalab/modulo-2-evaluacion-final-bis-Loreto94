2809d6d1ced3e56d927e2dc224afaa2028ca17e4
Loreto <loreto0105@gmail.com>
2025-03-23 16:43:47 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,9 @@
-console.log(">> Ready :)");const i=document.querySelector(".js-usersList"),a=()=>{fetch("https://randomuser.me/api/?results=10").then(t=>t.json()).then(t=>{console.log(t);const s=t.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value}));console.log(s),l(s);function l(e){let o="";for(const n of e)o+=`
-            <li class="js-user" id="${n.idValue}">
-                <img src="${n.photo}" alt="">
-                <h2>${n.name}</h2>
-                <p>${n.city}</p>
-                <p>${n.userName}</p>
+console.log(">> Ready :)");const r=document.querySelector(".js-usersList"),a=()=>{fetch("https://randomuser.me/api/?results=10").then(t=>t.json()).then(t=>{console.log(t);const l=t.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value}));console.log(l),c(l);function c(e){let s="";for(const o of e)s+=`
+            <li class="js-user" id="${o.idValue}">
+                <img src="${o.photo}" alt="">
+                <h2>${o.name}</h2>
+                <p>${o.city}</p>
+                <p>${o.userName}</p>
             </li>
-            `;i.innerHTML=o;const r=document.querySelectorAll(".js-user");for(const n of r)n.addEventListener("click",c)}function c(e){console.log("Ha hecho click");const o=e.currentTarget.id;console.log(o)}})};window.addEventListener("load",a);
+            `;r.innerHTML=s;const n=document.querySelectorAll(".js-user");for(const o of n)o.addEventListener("click",i)}function i(e){console.log("Ha hecho click");const s=e.currentTarget.id;console.log("ID del usuario clickado",s),console.log("usuarios favoritos");const n=l.find(o=>o.idValue===s);console.log(n),n&&(n.isFriend=!0,console.log("Usuario seleccionado",n),c(l))}})};window.addEventListener("load",a);
 //# sourceMappingURL=main.js.map
