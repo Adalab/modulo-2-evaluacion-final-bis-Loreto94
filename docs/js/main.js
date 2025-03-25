d273cfdbe226041f2456d836b0daf054adbb0ac1
Loreto <loreto0105@gmail.com>
2025-03-25 13:04:23 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,9 @@
-console.log(">> Ready :)");const a=document.querySelector(".js-usersList"),d=document.querySelector(".js-setBttn"),u=()=>{fetch("https://randomuser.me/api/?results=10").then(l=>l.json()).then(l=>{console.log(l);const t=l.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value}));console.log(t),c(t);function c(e){let n="";for(const o of e)n+=`
-            <li class="js-user" id="${o.idValue}">
-                <img src="${o.photo}" alt="">
-                <h2>${o.name}</h2>
-                <p>${o.city}</p>
-                <p>${o.userName}</p>
+console.log(">> Ready :)");const d=document.querySelector(".js-usersList"),u=document.querySelector(".js-setBttn"),m=document.querySelector(".js-recoverBttn"),g=()=>{fetch("https://randomuser.me/api/?results=10").then(s=>s.json()).then(s=>{console.log(s);const c=s.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value}));r(c);function r(e){let o="";for(const t of e)o+=`
+            <li class="js-user" id="${t.idValue}">
+                <img src="${t.photo}" alt="">
+                <h2>${t.name}</h2>
+                <p>${t.city}</p>
+                <p>${t.userName}</p>
             </li>
-            `;a.innerHTML=n;const s=document.querySelectorAll(".js-user");for(const o of s)o.addEventListener("click",i)}const r=()=>{localStorage.setItem("user",JSON.stringify(t))};d.addEventListener("click",r);function i(e){console.log("Ha hecho click");const n=e.currentTarget.id;console.log("ID del usuario clickado",n),console.log("usuarios favoritos");const s=t.find(o=>o.idValue===n);console.log(s),s&&(s.isFriend=!0,console.log("Usuario marcado"),c(t))}})};window.addEventListener("load",u);
+            `;d.innerHTML=o;const n=document.querySelectorAll(".js-user");for(const t of n)t.addEventListener("click",a)}const l=()=>{localStorage.setItem("user",JSON.stringify(c))},i=()=>{const e=JSON.parse(localStorage.getItem("user"));console.log(typeof e)};u.addEventListener("click",l),m.addEventListener("click",i);function a(e){console.log("Ha hecho click");const o=e.currentTarget.id;console.log("ID del usuario clickado",o),console.log("usuarios favoritos");const n=c.find(t=>t.idValue===o);console.log(n),n&&(n.isFriend=!0,console.log("Usuario marcado"),r(c))}})};window.addEventListener("load",g);
 //# sourceMappingURL=main.js.map
