30726a96517060c6aeef4effdb14f9afdeb31caf
Loreto <loreto0105@gmail.com>
2025-03-26 16:13:34 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,9 @@
-console.log(">> Ready :)");const d=document.querySelector(".js-usersList"),u=document.querySelector(".js-setBttn"),m=document.querySelector(".js-recoverBttn"),g=()=>{fetch("https://randomuser.me/api/?results=10").then(s=>s.json()).then(s=>{console.log(s);const c=s.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value}));r(c);function r(e){let o="";for(const t of e)o+=`
-            <li class="js-user" id="${t.idValue}">
-                <img src="${t.photo}" alt="">
-                <h2>${t.name}</h2>
-                <p>${t.city}</p>
-                <p>${t.userName}</p>
-            </li>
-            `;d.innerHTML=o;const n=document.querySelectorAll(".js-user");for(const t of n)t.addEventListener("click",a)}const l=()=>{localStorage.setItem("user",JSON.stringify(c))},i=()=>{const e=JSON.parse(localStorage.getItem("user"));console.log(typeof e)};u.addEventListener("click",l),m.addEventListener("click",i);function a(e){console.log("Ha hecho click");const o=e.currentTarget.id;console.log("ID del usuario clickado",o),console.log("usuarios favoritos");const n=c.find(t=>t.idValue===o);console.log(n),n&&(n.isFriend=!0,console.log("Usuario marcado"),r(c))}})};window.addEventListener("load",g);
+console.log(">> Ready :)");const l=document.querySelector(".js-usersList"),i=document.querySelector(".js-setBttn"),a=document.querySelector(".js-recoverBttn");let n=[];const d=()=>{fetch("https://randomuser.me/api/?results=10").then(t=>t.json()).then(t=>{console.log(t),n=t.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value})),r(n),i.addEventListener("click",m),a.addEventListener("click",g)})};function r(t){let e="";for(const o of t){const c=o.isFriend?"friend":"";e+=`
+<li class="user js-user ${c}" id="${o.idValue}">
+    <img src="${o.photo}" alt="">
+    <h2>${o.name}</h2>
+    <p>${o.city}</p>
+    <p>${o.userName}</p>
+</li>
+`}l.innerHTML=e;const s=document.querySelectorAll(".js-user");for(const o of s)o.addEventListener("click",u)}function u(t){console.log("Ha hecho click");const e=t.currentTarget.id;console.log("ID del usuario clickado",e),console.log("usuarios favoritos");const s=n.findIndex(o=>(console.log(e),o.idValue===e));s!==-1&&(n[s].isFriend=!0,console.log(n[s].isFriend),r(n))}const m=()=>{localStorage.setItem("user",JSON.stringify(n))},g=()=>{const t=JSON.parse(localStorage.getItem("user"));console.log(typeof t)};window.addEventListener("load",d);
 //# sourceMappingURL=main.js.map
