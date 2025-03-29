fa0eac0709ad1b9aefc46344dbfdc268efccd65b
Loreto <loreto0105@gmail.com>
2025-03-29 12:09:56 +0100

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,9 +1,10 @@
-console.log(">> Ready :)");const r=document.querySelector(".js-usersList"),i=document.querySelector(".js-setBttn"),a=document.querySelector(".js-recoverBttn");let o=[];const d=()=>{fetch("https://randomuser.me/api/?results=10").then(s=>s.json()).then(s=>{console.log(s),o=s.results.map(e=>({photo:e.picture.large,name:e.name.title+""+e.name.first+e.name.last,city:e.location.city,userName:e.login.username,isFriend:!1,idValue:e.id.value})),c(o),i.addEventListener("click",m),a.addEventListener("click",g)})};function c(s){let e="";for(const t of s){const l=t.isFriend?"friend":"";e+=`
-<li class="user js-user ${l}" id="${t.idValue}">
-    <img class="js-image" src="${t.photo}" alt="">
-    <h2 class="js-name">${t.name}</h2>
-    <p class="js-city">${t.city}</p>
-    <p class="js-username">${t.userName}</p>
+console.log(">> Ready :)");const l=document.querySelector(".js-usersList");document.querySelector(".js-setBttn");document.querySelector(".js-recoverBttn");let s=[];function r(o){let n="";for(const e of o){const c=e.isFriend?"friend":"";n+=`
+<li class="user js-user ${c}" id="${e.idValue}">
+    <img class="js-image" src="${e.photo}" alt="">
+    <h2 class="js-name">${e.name}</h2>
+    <p class="js-city">${e.city}</p>
+    <p class="js-username">${e.userName}</p>
+    <p>${e.phone}<p/>
 </li>
-`}r.innerHTML=e;const n=document.querySelectorAll(".js-user");for(const t of n)t.addEventListener("click",u)}function u(s){console.log("Ha hecho click");const e=s.currentTarget.id;console.log("ID del usuario clickado",e),console.log("usuarios favoritos");const n=o.findIndex(t=>(console.log(e),t.idValue===e));n!==-1&&(o[n].isFriend=!0,console.log(o[n].isFriend),c(o))}const m=()=>{localStorage.setItem("user",JSON.stringify(o))},g=()=>{const s=JSON.parse(localStorage.getItem("user"));console.log(typeof s),o=s,console.log(o),c(o)};window.addEventListener("load",d);
+`}l.innerHTML=n;const t=document.querySelectorAll(".js-user");for(const e of t)e.addEventListener("click",i)}function i(o){console.log("Ha hecho click");const n=o.currentTarget.id;console.log("ID del usuario clickado",n),console.log("usuarios favoritos");const t=s.findIndex(e=>(console.log(n),e.idValue===n));t!==-1&&(s[t].isFriend=!0,console.log(s[t].isFriend),r(s))}const u=()=>{const o=JSON.parse(localStorage.getItem("user"));console.log(typeof o),s=o,console.log(s),r(s)};u();
 //# sourceMappingURL=main.js.map
